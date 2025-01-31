1f3a8ed1f20bed2801471101ec04e73f674a6365
chrismarti343 <christianjosue321@gmail.com>
2025-01-30 19:39:09 -0500

    first commit

---
+++ src/main/java/test/Addtwoproducts.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Addtwoproducts  {
+
+
+    @Test
+    public void testAddTwoProducts() throws Exception {
+
+        System.out.println("Hello, I am testing here");
+
+    }
+
+}
