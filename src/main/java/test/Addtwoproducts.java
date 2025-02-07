472a9c2c31027dc895ea8f7849f1e3f58d1b2490
chrismarti343 <christianjosue321@gmail.com>
2025-02-06 21:23:25 -0500

    start posting a request

--- src/main/java/test/Addtwoproducts.java
+++ src/main/java/test/Addtwoproducts.java
@@ -1,8 +1,10 @@
 package test;
 
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
 
 public class Addtwoproducts  {
 
@@ -11,6 +13,22 @@ public class Addtwoproducts  {
     public void testAddTwoProducts() throws Exception {
 
         System.out.println("Hello, I am testing here");
+
+        String name = "Dodge";
+        String status = "available";
+        int id = 001;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": " + id + ",\n" +
+                        "  \"name\": \"" + name + "\",\n" +
+                        "  \"status\": \"" + status + "\"\n" +
+                        "}")
+                .when()
+                .post("https://petstore.swagger.io/v2/pet")
+                .then()
+                .statusCode(200);
 
     }
 
